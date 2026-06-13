e75bb427d95e9558007d92959749d9a25bdba880
Lucas Tavares <lucastavarestt@gmail.com>
2026-06-13 15:11:37 -0300

    Update Home component in `page.tsx` with new title and breadcrumb navigation
    - Changed the main heading to "Observatório Brasileiro de Governo Digital".
    - Added a subheading "Em construção..." for additional context.
    - Introduced `Breadcrumb` components for improved navigation structure.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,18 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Olá, mundo!</h1>
+      <h1 className="text-4xl font-bold">
+        Observatório Brasileiro de Governo Digital
+      </h1>
+      <h2>Em construção...</h2>
     </main>
   )
 }
