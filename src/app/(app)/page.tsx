143ab2487f02bc87fe59c1d0cfbc5844f072f9ac
Lucas Tavares <lucastavarestt@gmail.com>
2026-06-14 13:44:15 -0300

    Refactor Home component in `page.tsx` for improved layout and styling
    - Removed unused breadcrumb imports to streamline the component.
    - Adjusted the main heading size and alignment for better visual hierarchy.
    - Updated the main container's margin for enhanced responsiveness.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,15 +1,7 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-} from '@/components/ui/breadcrumb'
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">
+    <main className="flex mx-2 flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl text-center md:text-4xl font-bold">
         Observatório Brasileiro de Governo Digital
       </h1>
       <h2>Em construção...</h2>
